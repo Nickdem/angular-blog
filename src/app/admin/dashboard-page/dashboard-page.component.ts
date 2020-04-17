3236d11b0fc70a977b2b53a8d795b4666030d3a1
Nickdem <45073853+Nickdem@users.noreply.github.com>
2020-04-17 15:20:25 +0300

    Вывод постов, поиск и удаление

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PostsService } from 'src/app/shared/posts.service';
+import { Post } from 'src/app/shared/interfaces';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = []
+  pSub: Subscription
+  dSub: Subscription
+  searchStr: string = ""
+
+  constructor(private postService: PostsService) { }
 
   ngOnInit() {
+    this.pSub = this.postService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
   }
 
+  remove(id: string) {
+    this.dSub = this.postService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id)
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.pSub) {
+      this.pSub.unsubscribe()
+    }
+
+    if (this.dSub) {
+      this.dSub.unsubscribe()
+    }
+  }
 }
